4e4372d8c109ee63772ae5f1558e5c7e68275427
Jaipreet Singh <jaipreet.singh1@ontariotechu.net>
2022-01-22 17:14:33 -0500

    added ability to add book to library

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 let addBookButton = document.getElementById("add-book-btn");
+let bookForm = document.getElementById("book-form");
+let closeForm = document.querySelector(".close-form");
+let formSubmit = document.querySelector(".add-btn");
+let formBookName = document.getElementById("book-name");
+let formAuthorName = document.getElementById("author-name");
+let formNumPages = document.getElementById("num-pages");
+let formReadingProgress = document.getElementById("finished-reading");
 
 let myLibrary = [];
 
@@ -17,8 +24,33 @@ Book.prototype.info = function() {
     }
 }
 
-function addBookToLibrary() {
+function addBookToLibrary(book) {
+    let container = document.querySelector(".book-item-container");
+
+    let newBook = document.createElement("div");
+    newBook.classList.add("book-item");
+
+    newBook.innerHTML = `
+        <img class="book-delete" src="./images/x.png" alt="">
+        <div class="book-img-container">
+            <img class="book-img" src="./images/agameofthrones.jpg" alt="">
+        </div>
+        <h3>${book.name}</h3>
+        <p>${book.author}</p>
+        <p>${book.numPages} Pages</p>
+    `;
+
+    container.appendChild(newBook);
+}
+
+function resetForm() {
+    formBookName.value = "";
+    formAuthorName.value = "";
+    formNumPages.value = "";
+    formReadingProgress.value = "No"
     
+    let formContainer = document.querySelector(".form-container");
+    formContainer.style.visibility = "hidden";
 }
 
 // EVENT LISTENERS
@@ -26,4 +58,22 @@ function addBookToLibrary() {
 addBookButton.addEventListener('click', () => {
     let formContainer = document.querySelector(".form-container");
     formContainer.style.visibility = "visible";
+})
+
+bookForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    var bookName = formBookName.value;
+    var authorName = formAuthorName.value;
+    var numPages = formNumPages.value;
+    var readingProgress = formReadingProgress.value;
+
+    let book = new Book(bookName, authorName, numPages, readingProgress);
+    myLibrary.push(book);
+    addBookToLibrary(book);
+    resetForm();
+})
+
+closeForm.addEventListener('click', () => {
+    resetForm();
 })
